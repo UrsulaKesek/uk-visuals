e9f3a7fed0bc644dfcb88396faa89d2bc69e6869
Ursula Kesek <ursky@blueyonder.co.uk>
2020-06-27 12:41:25 +0100

    added letters

--- src/pages/GifSpriteFile/index.jsx
+++ src/pages/GifSpriteFile/index.jsx
@@ -1,27 +1,31 @@
 import React from "react";
 import Birds from "./Images/bird-anim-v.gif";
+import Letters from "./Images/aw-letters-9a.gif";
 import "./GifSprite.css";
 const GifSpriteFile = () => {
   return (
     <>
       <div class="bird">
         <img src={Birds} alt="robin-tweet-animation" />
-
-        <div class="gif">
+        <img src={Letters} alt="bonwit's-alphabet-animation" />
+        <div className="gif">
           <h2>The gif animation plays 3 times.</h2>
-          <p>1.I have used the robin png in two different sizes.</p>
-          <p>
-            2.Tweet is rendered using font-family Saira Stencil in 72pt and 30pt
-            font-sizes .I have used a strong contrasting color for the text
-            against a #68ae33 background.{" "}
+          <p>1.Using Photoshop I chose an artboard size 300px x 600px.This is one of the top
+            performing Ad sizes as suggested on the website for adsense.
           </p>
-          <p>3.As the birds hop about, so does the "tweet"</p>
+          <p>2.I have used the robin png in two sizes: 692px x 620px and 90px x 81px.</p>
           <p>
-            4.I have varied the delay on each frame to adjust the transition
+            3.Tweet is rendered using font-family Black Ops One in 72pt and 30pt
+            font-sizes .I have used a strong contrasting color #6b231a for the text
+            against a #68ae33 background.
+          </p>
+          <p>4.As the birds hop about, so does the "Tweet"</p>
+          <p>
+            5.I have varied the delay on each frame to adjust the transition
             speed through the animation.
           </p>
           <p>
-            5.The use of image and text together echos the original work in the
+            6.The use of image and text together echos the original work in the
             photos from Bonwit's Teller.
           </p>
         </div>
