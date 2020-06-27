55b34bb626005f4188c1cc62eea433e7ddf5d057
Ursula Kesek <ursky@blueyonder.co.uk>
2020-06-27 10:03:10 +0100

    edited gif

--- src/pages/GifSpriteFile/index.jsx
+++ src/pages/GifSpriteFile/index.jsx
@@ -1,21 +1,30 @@
 import React from "react";
-import Birds from "./Images/bird-anim-iv.gif";
+import Birds from "./Images/bird-anim-v.gif";
 import "./GifSprite.css";
 const GifSpriteFile = () => {
   return (
     <>
       <div class="bird">
         <img src={Birds} alt="robin-tweet-animation" />
-        
+
         <div class="gif">
-      <h2>The gif animation plays 3 times.</h2>
-      <p>1.I have used the robin png in two different sizes.</p>
-      <p>2.Tweet is rendered using font-family Saira Stencil.I have used a strong 
-        contrasting color for the text against a # background. </p>
-        <p>3.As the birds hop about, so does the "tweet"</p>
-        <p>4.The use of image and text together echos the original work
-           in the photos from Bonwit's Teller.</p>
-      </div>
+          <h2>The gif animation plays 3 times.</h2>
+          <p>1.I have used the robin png in two different sizes.</p>
+          <p>
+            2.Tweet is rendered using font-family Saira Stencil in 72pt and 30pt
+            font-sizes .I have used a strong contrasting color for the text
+            against a #68ae33 background.{" "}
+          </p>
+          <p>3.As the birds hop about, so does the "tweet"</p>
+          <p>
+            4.I have varied the delay on each frame to adjust the transition
+            speed through the animation.
+          </p>
+          <p>
+            5.The use of image and text together echos the original work in the
+            photos from Bonwit's Teller.
+          </p>
+        </div>
       </div>
     </>
   );
