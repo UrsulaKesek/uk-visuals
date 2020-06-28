5eeba8453482b832a205f5164e554ca3ce358be9
Ursula Kesek <ursky@blueyonder.co.uk>
2020-06-28 13:31:02 +0100

    Updates

---
+++ precache-manifest.84db4d25b28b81f5fa254b384ec7c177.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3655e57099fe524c33cb55c261f48b9e",
+    "url": "/uk-visuals/index.html"
+  },
+  {
+    "revision": "557cee2a50c0d04394c0",
+    "url": "/uk-visuals/static/css/main.365304c2.chunk.css"
+  },
+  {
+    "revision": "d50efd33fe1919ed5972",
+    "url": "/uk-visuals/static/js/2.5bc41f31.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/uk-visuals/static/js/2.5bc41f31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "557cee2a50c0d04394c0",
+    "url": "/uk-visuals/static/js/main.e7770a11.chunk.js"
+  },
+  {
+    "revision": "e2c7e8c1f7991a696d2c",
+    "url": "/uk-visuals/static/js/runtime-main.679b2b99.js"
+  },
+  {
+    "revision": "1ea24303abafb3e708a51ea012a414b7",
+    "url": "/uk-visuals/static/media/Magenta1q.1ea24303.png"
+  },
+  {
+    "revision": "14c6b44ccaf7db3f9b08d61434c36e84",
+    "url": "/uk-visuals/static/media/Perfume1.14c6b44c.jpeg"
+  },
+  {
+    "revision": "c67c91767b742dce88f43666e64a7b29",
+    "url": "/uk-visuals/static/media/Perfume2.c67c9176.jpeg"
+  },
+  {
+    "revision": "fa799015757dd12b980748fb62408b74",
+    "url": "/uk-visuals/static/media/Perfume4.fa799015.jpeg"
+  },
+  {
+    "revision": "105032a82cc2ed07f37d37d37f18523b",
+    "url": "/uk-visuals/static/media/aw-cat-2b.105032a8.jpg"
+  },
+  {
+    "revision": "0a5cceae7580110e7054765e07c77cf5",
+    "url": "/uk-visuals/static/media/aw-cat-3.0a5cceae.jpg"
+  },
+  {
+    "revision": "ecb682f49d95e1ac94d5d53819c8e69a",
+    "url": "/uk-visuals/static/media/aw-letters-11-1.ecb682f4.png"
+  },
+  {
+    "revision": "5d18f42587090dd295bcbbbd129cd723",
+    "url": "/uk-visuals/static/media/aw-letters-9a.5d18f425.png"
+  },
+  {
+    "revision": "0bc50558ca165a4687cd2a4d16699e43",
+    "url": "/uk-visuals/static/media/bird-anim-vi.0bc50558.gif"
+  },
+  {
+    "revision": "b250365cf04b1b1f32b8f90dd61d69fc",
+    "url": "/uk-visuals/static/media/blue-cat.b250365c.jpg"
+  },
+  {
+    "revision": "7d5095dbce45041db2b7dd551e77056b",
+    "url": "/uk-visuals/static/media/clubs128x128.7d5095db.png"
+  },
+  {
+    "revision": "ada3646017aebfbbddb31331a083424c",
+    "url": "/uk-visuals/static/media/clubs256x256.ada36460.png"
+  },
+  {
+    "revision": "b40ca89c008cbf1b5514065ef4330fcd",
+    "url": "/uk-visuals/static/media/mobile-shop-front-1iv.b40ca89c.png"
+  },
+  {
+    "revision": "fe17d899b43de003c7027e6fa26d4e0f",
+    "url": "/uk-visuals/static/media/mobile-shop-front-viii.fe17d899.png"
+  },
+  {
+    "revision": "e9c9e6e92ed8456abcd197326c0613b9",
+    "url": "/uk-visuals/static/media/pb11b.e9c9e6e9.png"
+  },
+  {
+    "revision": "5c9094401b0865286a6b70b1fb283f68",
+    "url": "/uk-visuals/static/media/pb75x105.5c909440.png"
+  },
+  {
+    "revision": "3590ea81a76634470a68b56c0a7edd3b",
+    "url": "/uk-visuals/static/media/perfume-project-k.3590ea81.png"
+  },
+  {
+    "revision": "981675a1cce39e107e0370ae408a7c62",
+    "url": "/uk-visuals/static/media/robin-180x162.981675a1.png"
+  },
+  {
+    "revision": "a241b92dd95d0470d934f27a7e298a2a",
+    "url": "/uk-visuals/static/media/robin3.a241b92d.png"
+  },
+  {
+    "revision": "c7903a8f788680635dd2f039595a411b",
+    "url": "/uk-visuals/static/media/tightrope5.c7903a8f.jpg"
+  }
+]);
