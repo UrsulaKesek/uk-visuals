6bbab893bc31cebce58c2bb0dd042fa208ec5a44
Ursula Kesek <ursky@blueyonder.co.uk>
2020-06-30 13:55:33 +0100

    edited app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
-import { AssetsFile,GifSpriteFile,HomePage,MockUps } from "./pages";
+import { AssetsFile, GifSpriteFile, HomePage, MockUps } from "./pages";
 
 function App() {
   return (
     <>
-    <Switch>
-      <Route path="/home" exact component={HomePage} />
-      <Route path="/mockups" exact component={MockUps} />
-      <Route path="/assets" exact component={AssetsFile} />
-      <Route path="/gif" exact component={GifSpriteFile} />
-    </Switch>
+      <div className="view">Best with FULL SCREEN view</div>
+      <Switch>
+        <Route path="/home" exact component={HomePage} />
+        <Route path="/mockups" exact component={MockUps} />
+        <Route path="/assets" exact component={AssetsFile} />
+        <Route path="/gif" exact component={GifSpriteFile} />
+      </Switch>
     </>
   );
 }
