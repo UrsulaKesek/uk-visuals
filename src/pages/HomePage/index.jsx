c79e7dca87aa5e891fc5dd23c6dd96cec8c4b27c
Ursula Kesek <ursky@blueyonder.co.uk>
2021-12-06 16:16:11 +0000

    edited homepage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -4,14 +4,14 @@ import "./HomePage.css";
 
 const HomePage = () => 
 <main className="home">
-    <>
+    
     <h1>Please use FULL SCREEN VIEW when viewing the files.</h1>
     <p>The reasoning behind the work here is to do with being in lockdown.
         I became very aware of missing going out and putting on perfume and also 
         shopping for perfumes or having scented candles at home.
         I was looking for images of perfume shop windows and by accident came across the photographs
         you see here. The rest, as they say is history!  </p>
-        </>
+        
 </main>
 
 export default HomePage;
